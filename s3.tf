eea15dc5696f18b1512af5b055e8246dd822e8ae
AjayGitHub08 <93314477+AjayGitHub08@users.noreply.github.com>
2025-11-16 11:59:03 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "ajayBuckets.monobucket"
+  bucket = "ajaybuckets.monobucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -27,7 +27,7 @@ status = "Enabled"
 #terraform {
 #backend "s3" {
 #region = "eu-north-1"
-#bucket = "ajayBuckets.monobucket"
+#bucket = "ajaybuckets.monobucket"
 #key = "prod/terraform.tfstate"
 #}
 #}
